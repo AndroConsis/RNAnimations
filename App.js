d418fa23d14fa2f496c2736d5203269821f12f28
Prateek Rathore <prateek@Prateeks-MacBook-Pro.local>
2018-08-02 21:22:28 +0530

    Setting Offset on onPanResponderGrant Event

--- App.js
+++ App.js
@@ -15,18 +15,36 @@ export default class App extends Component {
   }
 
   componentWillMount() {
+    this._x = 0;
+    this._y = 0;
+
+    this.state.animation.addListener((value) => {
+      this._x = value.x;
+      this._y = value.y;
+    })
     this._panResponder = PanResponder.create({
       onStartShouldSetPanResponder: () => true,
       onMoveShouldSetPanResponder: () => true,
+      onPanResponderGrant: () => {
+        this.state.animation.setOffset({
+          x: this._x,
+          y: this._y
+        });
+
+        this.state.animation.setValue({
+          x: 0,
+          y: 0
+        });
+      },
       onPanResponderMove: Animated.event([
         null, {
           dx: this.state.animation.x,
           dy: this.state.animation.y
         }
       ]),
-      onPanResponderRelease: (e, {vx, vy}) => {
+      onPanResponderRelease: (e, { vx, vy }) => {
         Animated.decay(this.state.animation, {
-          velocity: {x: vx, y: vy},
+          velocity: { x: vx, y: vy },
           deceleration: 0.995
         }).start();
       },
@@ -35,15 +53,15 @@ export default class App extends Component {
 
   render() {
     const animatedStyle = {
-      transform: 
-      this.state.animation.getTranslateTransform(),
+      transform:
+        this.state.animation.getTranslateTransform(),
     }
 
     return (
       <View style={styles.container}>
-        <Animated.View 
-        style={[styles.box, animatedStyle]} 
-        {...this._panResponder.panHandlers}
+        <Animated.View
+          style={[styles.box, animatedStyle]}
+          {...this._panResponder.panHandlers}
         />
       </View>
     );
